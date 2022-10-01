64b9e11b6b3d23a35476238dda558c5a55c7d525
gangnamsai <114744369+gangnamsai@users.noreply.github.com>
2022-10-01 16:29:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function ClearFields(){
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("confirm_password").value = "";
+}
 function submit(){
     alert("Success!")
 }
