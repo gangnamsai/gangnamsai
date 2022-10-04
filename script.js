90035f1114bc2e7de643cd87fba98aa008bb7dac
gangnamsai <114744369+gangnamsai@users.noreply.github.com>
2022-10-04 18:46:29 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function ClearFields(){
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
-    document.getElementById("confirm_password").value = "";
+    document.getElementById("email_id").value = "";
+    document.getElementById("password_id").value = "";
+    document.getElementById("message").value = "";
 }
 function submit(){
     alert("Success!")
 }
+
+/*Email Js */
+const btn = document.getElementById('button');
+
+document.getElementById('contact-form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_7t8p6eg';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send';
+      alert(JSON.stringify(err));
+    });
+});
